6732e5aedb6ffb27de0bcbdce93a76149406312d
JaharaKate <21-01880@g.batstate-u.edu.ph>
2024-04-09 10:18:21 +0800

    firebase connectivity test

---
+++ src/components/AdminPage0.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Welcome to the AdminPage 0</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AdminPage0',
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage',
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
