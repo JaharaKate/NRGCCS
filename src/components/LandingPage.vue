79afbefdaac4aa83558a5345f489a5febc0a0d24
John Michael Gabalfin <21-00856@g.batstate-u.edu.ph>
2024-04-16 22:21:46 +0800

    Revert "removed folder view"
    This reverts commit 02adeb3495f22a0f3609cd2f6fa65e2ba458e7cc.

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    <h1>Welcome to the LandingPage</h1>
-  </div>
+  <v-app-bar app color="#AD0606" dark flat class="px-12">
+    <v-img src ="../"></v-img>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-white">Home</v-btn>
+    <v-btn text @click="scroll('about')" class="text-white">About</v-btn>
+  </v-app-bar>
 </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage',
 
-<script>
-export default {
-  name: "LandingPage",
-};
-</script>
-
-<style scoped></style>
+    methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+  
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
