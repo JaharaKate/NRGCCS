f27067a05e72ef072db02bdfd940ff305e19fbb2
John Michael Gabalfin <21-00856@g.batstate-u.edu.ph>
2024-04-16 22:22:00 +0800

    Revert "firebase connectivity test"
    This reverts commit 6732e5aedb6ffb27de0bcbdce93a76149406312d.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; 
+import './assets/main.css'
 
-createApp(App).use(router).mount('#app'); 
+import { createApp } from 'vue'
+import App from './App.vue' //This stays red, but still works?
+
+createApp(App).mount('#app')
