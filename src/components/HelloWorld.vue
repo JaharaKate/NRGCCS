cfe4bc0852d80cf9f8ae8ade0acda2f298c7fed5
John Michael Gabalfin <21-00856@g.batstate-u.edu.ph>
2024-04-16 22:21:32 +0800

    Revert "revert"
    This reverts commit 60c470955adfc27a0ce20482384ddfcd21fd84b4.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,4 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
-</template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
-
+<<<<<<< HEAD
 <<<<<<< HEAD
 <template>
   <v-container>
@@ -195,6 +154,8 @@ export default defineComponent({
 })
 </script>
 =======
+=======
+>>>>>>> parent of 6732e5a (firebase connectivity test)
 <script setup lang="ts">
 defineProps<{
   msg: string
@@ -236,4 +197,7 @@ h3 {
   }
 }
 </style>
+<<<<<<< HEAD
+>>>>>>> parent of 6732e5a (firebase connectivity test)
+=======
 >>>>>>> parent of 6732e5a (firebase connectivity test)
