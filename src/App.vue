5a25d4d11f9c7cbe2edcf3fe5771c9ed473c0729
hazzeeella <hazelanneagapito@gmail.com>
2024-04-16 20:50:44 +0800

    add vuetify and folder view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-      <h1>Welcome to the Landing Page</h1>
-    </div>
-  </template>
+  <v-app-bar app color="#AD0606" dark flat class="px-12">
+    <v-img src ="../"></v-img>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-white">Home</v-btn>
+    <v-btn text @click="scroll('about')" class="text-white">About</v-btn>
+  </v-app-bar>
+</template>
   
   <script>
   export default {
     name: 'LandingPage',
+
+    methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+  
   };
   </script>
   
