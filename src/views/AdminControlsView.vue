655e359f5aef67cd400a06dea5dab37d68f67ec2
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-05 17:07:35 +0800

    Added AdminView

---
+++ src/views/AdminControlsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AdminHeader from "../components/AdminHeader.vue";
+</script>
+
+<template>
+  <main>
+    <AdminHeader />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
