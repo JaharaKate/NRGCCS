e1e72298f2e4ba9a28049b8e873d301d0aa94414
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-06 18:08:50 +0800

    Imported material design icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
