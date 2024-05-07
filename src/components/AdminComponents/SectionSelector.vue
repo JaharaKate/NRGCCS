9ad133818b6e62e9e4412de2f701c528000b7365
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-07 11:48:59 +0800

    Buttons now switch

--- src/components/AdminComponents/SectionSelector.vue
+++ src/components/AdminComponents/SectionSelector.vue
@@ -1,32 +1,53 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const test = () => {};
+const isSelected = ref(0);
+const adminClick = () => {
+  isSelected.value = 0;
+};
+const moduleClick = () => {
+  isSelected.value = 1;
+};
+</script>
 
 <template>
   <div class="container">
-    <div class="item" id="selected">
+    <button
+      class="item"
+      :class="{ selected: isSelected == 0 }"
+      @click="adminClick()"
+    >
       <v-icon icon="mdi-account" />Admin Manager
-    </div>
-    <div class="item"><v-icon icon="mdi-book-edit" /> Module Manager</div>
+    </button>
+    <button
+      class="item"
+      :class="{ selected: isSelected == 1 }"
+      @click="moduleClick"
+    >
+      <v-icon icon="mdi-book-edit" /> Module Manager
+    </button>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
-  margin-top: 20%;
+  margin-top: 5vh;
   display: flex;
   flex-direction: column;
-  font-size: 1.5em;
+  font-size: 1.3em;
 }
 .item {
   display: flex;
   justify-content: space-around;
   width: 100%;
-  margin-top: 2%;
+  padding-top: 2%;
   padding: 4%;
   border-bottom-style: solid;
   border-bottom-width: 2px;
   border-bottom-color: #9e9e9e;
 }
-#selected {
+.selected {
   color: white;
   border-bottom-style: none;
   background-color: #ad0606;
