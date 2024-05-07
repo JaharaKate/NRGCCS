3d078fdbb68a93d7f4b06b80e6491dbd1b57b0e7
John Michael Gabalfin <21-00856@g.batstate-u.edu.ph>
2024-05-08 05:11:34 +0800

    LessonSlots(Test-Unfixed)

---
+++ src/components/LessonSlots.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="lesson">
+        <p>
+            <slot name="lesson-title"></slot>
+        </p>
+    </div>
+    <div class="lesson-body">
+        <div class="items">
+            <slot name="lesson-img">
+                <img src="/src/assets/img/course1.png">
+            </slot>
+            <p class="lesson-sub">
+                <slot name="lesson-caption"></slot>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.lesson{
+  position: relative;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  width: 100vw;
+  height: 10vw;
+  margin-top: 67px;
+}
+p {
+    padding-top: 2%;
+    position: absolute;
+    font-family: "Arapey", serif;
+    font-size: 3em;
+    font-style: normal;
+    color: black;
+    z-index: 100;
+  }
+.lesson-body{
+    position:relative;
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-content: space-between;
+    width: 100vw;
+    height: 30vw;
+    background-color: aqua;
+}
+.lesson-img{
+    width: 50px
+}
+.lesson-sub{
+
+}
+</style>
--- src/views/CourseViews/LessonView.vue
+++ src/views/CourseViews/LessonView.vue
@@ -1,11 +1,17 @@
 <script setup>
 import Header from "@/components/Header.vue";
-import Banner from "@/components/Banner.vue";
+import LessonSlots from "@/components/LessonSlots.vue";
 </script>
 
 <template>
   <Header class="header" />
-  <Banner />
+  <LessonSlots>
+    <template #lesson-title>Lesson 1 - A Walk that is Always Worthwhile</template>
+    <template #lesson-img>
+      <img class="aba" src="/src/assets/img/course2.png"/>
+    </template>
+    <template #lesson-caption>Lesson 1 - A Walk that is Always Worthwhile</template>
+  </LessonSlots>
 </template>
 
 <style lang="scss" scoped>
@@ -16,6 +22,9 @@ import Banner from "@/components/Banner.vue";
   left: 0;
   width: 100%;
   z-index: 200;
+}
+.aba{
+  width: 50vw;
 }
 
 </style>
