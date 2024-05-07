82e105f9d823efadbb77db8441ec77ef9acea526
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-07 11:48:44 +0800

    Line Tweaks

--- src/router/index.js
+++ src/router/index.js
@@ -20,13 +20,10 @@ const router = createRouter({
     {
       path: "/admin/login",
       name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/LogInView.vue"),
     },
     {
-      path: "/admin",
+      path: "/admin/",
       name: "admin",
       component: () => import("../views/AdminControlsView.vue"),
     },
