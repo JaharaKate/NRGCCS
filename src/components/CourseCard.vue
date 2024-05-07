7b23785a01fb311a90754bbc53863d9b7c963ad0
hazzeeella <hazelanneagapito@gmail.com>
2024-05-08 00:52:49 +0800

    CourseCard Edited

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,73 +1,28 @@
 <script setup></script>
 
 <template>
-  <v-container fluid fill-height>
-    <v-row justify="center" align="center" class="course-background">
-      <v-col cols="6">
-        <v-card>
-          <v-img
-            class="bg-grey-lighten-2"
-            height="220"
-            src="/src/assets/img/course1.png"
-            cover
-          ></v-img>
-          <v-card-title class="text-h6">
-            English For Academic Purposes
-          </v-card-title>
-        </v-card>
-      </v-col>
-
-      <v-col cols="6">
-        <v-card>
-          <v-img
-            class="bg-grey-lighten-2"
-            height="220"
-            src="/src/assets/img/course2.png"
-            cover
-          ></v-img>
-          <v-card-title class="text-h6"> Everyday English </v-card-title>
-        </v-card>
-      </v-col>
-
-      <v-col cols="6">
-        <v-card>
-          <v-img
-            class="bg-grey-lighten-2"
-            height="220"
-            src="/src/assets/img/course3.png"
-            cover
-          ></v-img>
-          <v-card-title class="text-h6">
-            English For Professional <br />Communication
-          </v-card-title>
-        </v-card>
-      </v-col>
-
-      <v-col cols="6">
-        <v-card>
-          <v-img
-            class="bg-grey-lighten-2"
-            height="220"
-            src="/src/assets/img/course4.png"
-            cover
-          ></v-img>
-          <v-card-title class="text-h6">
-            English For Global and Intercultural <br />Communication
-          </v-card-title>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="course-card">
+    <v-card>
+      <v-img
+        class="course1"
+        max-height="200"
+        src="/src/assets/img/course1.png"
+        cover
+      ></v-img>
+      <v-card-title class="course-text"> EverydayEnglish </v-card-title>
+    </v-card>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.v-card {
+.course-card {
   width: 500px;
-  margin-top: 50px;
-  margin-left: 100px;
+  height: 600vh;
+  display: inline-block;
 }
 
-.v-img {
-  height: 200px !important;
+.course1 {
+  width: 100%;
+  height: 200vh;
 }
 </style>
--- src/views/CourseViews/CourseView.vue
+++ src/views/CourseViews/CourseView.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import Banner from "@/components/Banner.vue";
+import CourseCard from "@/components/CourseCard.vue";
 </script>
 
 <template>
   <Header class="header" />
   <Banner />
+  <div class="course-view-content">
+    <CourseCard />
+    <!-- Add more CourseCard components as needed -->
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -17,5 +22,8 @@ import Banner from "@/components/Banner.vue";
   width: 100%;
   z-index: 200;
 }
-
+.course-view-content {
+  padding-top: 100px;
+  margin: 10px; /* Adjust as needed to avoid overlapping with header */
+}
 </style>
