d8930bddcc54122062e3e9a6ce618edf10cb600c
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-07 11:49:34 +0800

    Moved AdminBody here.

--- src/views/AdminControlsView.vue
+++ src/views/AdminControlsView.vue
@@ -1,13 +1,44 @@
 <script setup>
 import AdminHeader from "../components/AdminComponents/AdminHeader.vue";
-import AdminBody from "../components/AdminComponents/AdminBody.vue";
+import SectionSelector from "../components/AdminComponents/SectionSelector.vue";
 </script>
 
 <template>
   <main>
     <AdminHeader />
-    <AdminBody />
+    <div id="box-container">
+      <div id="side-container"><SectionSelector /></div>
+      <div id="main-container"></div>
+    </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montagu+Slab:opsz,wght@16..144,100..700&display=swap");
+body {
+  font-family: "Montagu Slab", serif;
+}
+#box-container {
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  height: 100vh;
+  width: 1140px;
+}
+p {
+  font-size: larger;
+  color: red;
+  margin-top: 50px;
+}
+#side-container {
+  background-color: #d9d9d9;
+  height: 100%;
+  width: 20vw;
+  box-shadow: inset gray -40px 0px 40px -40px;
+}
+#main-container {
+  background-color: #f2f2f2;
+  height: 100%;
+  width: 100%;
+}
+</style>
