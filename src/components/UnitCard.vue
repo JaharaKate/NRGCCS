e121029a8b5cdb7c988dd1336ad4c39a67d6d03a
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2024-05-09 11:23:20 +0800

    Unit Card Component

---
+++ src/components/UnitCard.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <v-card elevation="5" class="unit-card" :ripple="false">
+    <v-card-title primary-title class="card-title">
+      <slot></slot>
+    </v-card-title>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+.unit-card {
+  display: flex;
+  padding-left: 1%;
+  width: 70%;
+  box-shadow: 20px;
+  cursor: pointer;
+}
+.card-title {
+  font-size: 2em;
+}
+</style>
